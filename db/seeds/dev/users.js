5dd17de99d5460052ca79b38ccb4d986a139ed8e
MackHalliday <mackenzie.halliday@gmail.com>
2019-11-20 12:04:21 -0700

    Update database and code in favorites post to return

--- db/seeds/dev/users.js
+++ db/seeds/dev/users.js
@@ -10,13 +10,15 @@ exports.seed = function(knex) {
       return Promise.all([
 
         // Insert a single paper, return the paper ID, insert 2 favorites
-        knex('users').insert({
-          api_key: '123'
-        }, 'id')
+        knex('users').insert([
+          { id: 1 , api_key: '123'},
+          { id: 2 , api_key: '456'}])
         .then(users => {
           return knex('favorites').insert([
-            { location: 'denver,co', user_id: users[0] },
-            { location: 'chicago,il', user_id: users[0] }
+            { location: 'denver,co', user_id: 1 },
+            { location: 'chicago,il', user_id: 1 },
+            { location: 'denver,co', user_id: 2 },
+            { location: 'chicago,il', user_id: 2 }
           ])
         })
         .then(() => console.log('Seeding complete!'))
--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -5,16 +5,16 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 
+//asynchronous function
 router.get('/', (request, response) => {
-  let apiKey = JSON.stringify(request.body.api_key)
-  // let userId = database('users').where({api_key: apiKey})
-  // let userId = database('users').select('id').where({api_key: apiKey})
-  let userId = '41'
 
-  console.log(apiKey)
-  console.log(userId)
+  // await
+  // need to handle promise -> database
+  let userId = database('users').where('api_key', request.body.api_key)
 
-  database('favorites').select('location').where({user_id: userId})
+  console.log(request.body.api_key)
+
+   database('favorites').select('location').where({'user_id': userId})
     .then((favorites) => {
       response.status(200).json(favorites);
     })
