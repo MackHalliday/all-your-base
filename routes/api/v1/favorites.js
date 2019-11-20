4437c62a2156088592522c920080968c74e0fa3d
MackHalliday <mackenzie.halliday@gmail.com>
2019-11-20 00:00:39 -0700

    Update to find user thru api key passed in body

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -5,9 +5,11 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 
-
 router.get('/', (request, response) => {
-  database('favorites').select()
+  let apiKey = JSON.stringify(request.body.api_key)
+  let userId = database('users').where({api_key: apiKey})
+
+  database('favorites').select('location').where({user_id: userId})
     .then((favorites) => {
       response.status(200).json(favorites);
     })
