9bddcd4054f43fc5a194df305e59e8c0859ad66e
MackHalliday <mackenzie.halliday@gmail.com>
2019-11-20 23:30:23 -0700

    Add validation for api_key

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -8,23 +8,27 @@ const database = require('knex')(configuration);
 let GeoCodingService = require('../../../services/geo_coding_service.js');
 let DarkSkyService = require('../../../services/dark_sky_service.js');
 let UserObject = require('../../../models/user_object.js');
+let ForecastObject = require('../../../models/forecast_object.js');
 
 let geoCodingService = new GeoCodingService();
 let darkSkyService = new DarkSkyService();
 let userObject = new UserObject();
 
-router.get('/', async function (request, response, next) {
+router.get('/', async function (request, response) {
 
   let api_key = request.body.api_key
   let valid_key = await userObject.validKey(api_key)
 
-  if (valid_key === true){
+  if (valid_key === true ){
+    
     let location = await request.query.location
     let coordinates = await geoCodingService.getCoordinatesAsync(location);
     let forecast = await darkSkyService.getForecast(coordinates);
-    return response.status(200).json(forecast);
+    let forcastFormat = await new ForecastObject(location, forecast);
+
+    return response.status(200).json(forcastFormat);
   } else {
-    return response.status(404).send("error");
+    return response.status(404).send("Unauthorized");
   };
 })
 
