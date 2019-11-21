44b375ee379087970b9839b5adc7898906d9a368
MackHalliday <mackenzie.halliday@gmail.com>
2019-11-20 21:08:35 -0700

    Minor change to spacing

--- services/geo_coding_service.js
+++ services/geo_coding_service.js
@@ -5,7 +5,6 @@ class GeoCodingService {
     this.baseUrl = "https://maps.googleapis.com/maps/api"
   }
 
-
   async getCoordinatesAsync(location){
     let response = await fetch(`${this.baseUrl}/geocode/json?address=${location}&key=${process.env.GOOGLE_API_KEY}`)
     let data = await response.json()
