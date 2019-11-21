1ea5d9cbdac1195cb19237910c0fef48698dca6d
MackHalliday <mackenzie.halliday@gmail.com>
2019-11-20 19:48:53 -0700

    get function now async, add functionality to call geoCode and darkSky services

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -8,15 +8,15 @@ const database = require('knex')(configuration);
 let GeoCodingService = require('../../../services/geo_coding_service.js');
 let geoCodingService = new GeoCodingService();
 
-router.get('/', (request, response) => {
-  let location = request.query.location
-  let coordinates = geoCodingService.getCoordinates(location)
-    .then((location) => {
-      response.status(200).json(papers);
-    })
-    .catch((error) => {
-      response.status(500).json({ error });
-    });
-});
+let DarkSkyService = require('../../../services/dark_sky_service.js');
+let darkSkyService = new DarkSkyService();
+
+router.get('/', async function (request, response, next) {
+  const location = await request.query.location
+  const coordinates = await geoCodingService.getCoordinatesAsync(location);
+  const forecast = await darkSkyService.getForecast(coordinates);
+
+  response.status(200).json(forecast);
+})
 
 module.exports = router;
