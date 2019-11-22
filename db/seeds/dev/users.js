d79425a03247997bff99cfd4b95c45266be53a8f
MackHalliday <mackenzie.halliday@gmail.com>
2019-11-21 17:59:04 -0700

    Add additional user and favorite locations to seed

--- db/seeds/dev/users.js
+++ db/seeds/dev/users.js
@@ -5,13 +5,16 @@ exports.seed = function(knex) {
       return Promise.all([
         knex('users').insert([
           { id: 1 , api_key: '123'},
-          { id: 2 , api_key: '456'}])
+          { id: 2 , api_key: '456'},
+          { id: 3 , api_key: '789'}])
         .then(users => {
           return knex('favorites').insert([
             { location: 'denver,co', user_id: 1 },
             { location: 'chicago,il', user_id: 1 },
+            { location: 'seattle,wa', user_id: 2 },
+            { location: 'bloomington,il', user_id: 2 },
             { location: 'denver,co', user_id: 2 },
-            { location: 'chicago,il', user_id: 2 }
+            { location: 'paris,france', user_id: 3 }
           ])
         })
         .then(() => console.log('Seeding complete!'))
